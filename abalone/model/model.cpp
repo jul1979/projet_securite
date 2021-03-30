c0a543bc2ddb9936d0d7a388e8c70fe3b9cf9f3c
Ruzindana-Rukundo jules <48982@etu.he2b.be>
2021-03-30 11:35:56 +0200

    subdir prêt à l'emploi

---
+++ abalone/model/model.cpp
@@ -0,0 +1,10 @@
+#include "model.h"
+
+Model::Model()
+{
+}
+
+int Model::sum(int a, int b)
+{
+ return a+b;
+}
---
+++ abalone/model/model.h
@@ -0,0 +1,11 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+class Model
+{
+public:
+    Model();
+    int sum(int,int);
+};
+
+#endif // MODEL_H
